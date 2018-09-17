38f1b3b4129fe91c687f71817511eb5d4d4ed57e
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-09-17 09:01:59 -0700

    Update port

--- app/app.py
+++ app/app.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def dump_headers(path):
   return '{}\n\nHEADERS:\n{}\nHOSTNAME:{}'.format(request.method + ' ' + request.url, str(request.headers), platform.node()), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
