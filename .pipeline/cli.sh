0200b9a2a0f15b45cc45709d9d931e1dd4e54b98
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-10-25 21:40:42 -0700

    update cli version

--- .pipeline/cli.sh
+++ .pipeline/cli.sh
@@ -4,4 +4,4 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd $DIR
-curl -sSL 'https://raw.githubusercontent.com/cvarjao-o/node-oc-cli-wrapper/0bad141a71e732d15d35b9c6ff82ee1c55054917/cli.sh' | bash -s "$@"
+curl -sSL 'https://raw.githubusercontent.com/cvarjao-o/node-oc-cli-wrapper/094654871b419a02f1e79a6a1c7cdb13b4a12368/cli.sh' | bash -s "$@"
