a0daf818ac850f8eff6d8e3843034f745b9be180
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-11-22 13:54:29 -0800

    Update config.groovy

--- jenkins/config.groovy
+++ jenkins/config.groovy
@@ -1,7 +1,6 @@
-
 app {
-    name = "${opt.'name'?:'jenkins-hello-world'}"
-    namespaces {
+    name = "${opt.'name'?:'jenkins'}"
+    namespaces { //can't call environments :(
         'build'{
             namespace = "${opt.'namespace'?:'csnr-devops-lab-tools'}"
             disposable = true
@@ -19,7 +18,7 @@ app {
     git {
         workDir = ['git', 'rev-parse', '--show-toplevel'].execute().text.trim()
         uri = ['git', 'config', '--get', 'remote.origin.url'].execute().text.trim()
-        ref = ['bash','-c', 'git config branch.`git name-rev --name-only HEAD`.merge'].execute().text.trim()
+        ref = "refs/pull/${opt.'pr'}/head"
         commit = ['git', 'rev-parse', 'HEAD'].execute().text.trim()
     }
 
@@ -28,10 +27,11 @@ app {
             name = "build"
             id = "pr-${opt.'pr'}"
         }
-        version = "${app.build.env.name}-v${opt.'pr'}"
-        name = "${opt.'build-name'?:app.name}"
         suffix = "-build-${opt.'pr'}"
         id = "${app.name}${app.build.suffix}"
+        version = "${app.build.env.name}-v${opt.'pr'}"
+        name = "${opt.'build-name'?:app.name}"
+
         namespace = app.namespaces.'build'.namespace
         timeoutInSeconds = 60*20 // 20 minutes
         templates = [
@@ -55,23 +55,28 @@ app {
             name = vars.deployment.env.name // env-name
             id = vars.deployment.env.id
         }
+        suffix = "${vars.deployment.suffix}" // app (unique name across all deployments int he namespace)
         version = "${vars.deployment.version}" //app-version  and tag
-        name = "${vars.deployment.name}" //app-name   (same name accross all deployments)
+        name = "${vars.deployment.name}"
         id = "${app.deployment.name}${app.deployment.suffix}" // app (unique name across all deployments int he namespace)
-        namespace = "${vars.deployment.namespace}"
 
+        namespace = "${vars.deployment.namespace}"
         timeoutInSeconds = 60*20 // 20 minutes
+        host = "${app.deployment.id}-${app.deployment.namespace}.pathfinder.gov.bc.ca"
+
         templates = [
-                [
-                    'file':'https://raw.githubusercontent.com/cvarjao-o/openshift-templates/f1d3e44018618ef7c9b9b52dd81d83d7030115db/jenkins/jenkins.dc.yaml',
-                    'params':[
-                        'NAME':app.deployment.name,
-                        'BC_NAME':app.build.name,
-                        'NAME_SUFFIX':'cvarjao',
-                        'VERSION': app.deployment.version,
-                        'ROUTE_HOST': 'jenkinns-hello-cvarjao.pathfinder.gov.bc.ca'
-                    ]
+            [
+                'file':'jenkins/openshift/jenkins.dc.json',
+                'params':[
+                    'NAME': "${app.deployment.name}",
+                    'BC_NAME': "${app.build.name}",
+                    'SUFFIX': "${app.deployment.suffix}",
+                    'VERSION': app.deployment.version,
+                    'ROUTE_HOST': app.deployment.host,
+                    'ENV_NAME':app.deployment.env.name,
+                    'ENV_ID':app.deployment.env.id
                 ]
+            ]
         ]
     }
 }
@@ -121,4 +126,3 @@ environments {
         }
     }
 }
-
