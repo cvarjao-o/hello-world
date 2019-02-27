6ffec56caa0e4f4c89fdf561a934d3c4775504f3
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2019-02-26 23:24:48 -0800

    Update config.js

--- .jenkins/.pipeline/lib/config.js
+++ .jenkins/.pipeline/lib/config.js
@@ -4,11 +4,15 @@ const changeId = options.pr //aka pull-request
 const version = 'v2.0'
 const NAME = 'jenkins'
 
+if (options.pr == null){
+  throw new Error("Missing --pr argument")
+}
+
 const phases = {
-  build: {namespace:'csnr-devops-lab-tools'  , name: NAME, phase: 'build', changeId:changeId, suffix: `-build-${changeId}`, instance: `${NAME}-build-${changeId}`, version:`${version}-${changeId}`, tag:`${version}-${changeId}`},
-    dev: {namespace:'csnr-devops-lab-tools' , name: NAME, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${NAME}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`${version}-${changeId}`},
-   test: {namespace:'csnr-devops-lab-tools' , name: NAME, phase: 'test' , changeId:changeId, suffix: '-test'             , instance: `${NAME}-test`             , version:`${version}-${changeId}`, tag:`${version}`},
-   prod: {namespace:'csnr-devops-lab-tools' , name: NAME, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${NAME}-prod`             , version:`${version}-${changeId}`, tag:`${version}`}
+  build: {namespace:'csnr-devops-lab-tools' , name: NAME, phase: 'build', changeId:changeId, suffix: `-build-${changeId}`, instance: `${NAME}-build-${changeId}`, tag:`build-${version}-${changeId}`},
+    dev: {namespace:'csnr-devops-lab-tools' , name: NAME, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${NAME}-dev-${changeId}`  , tag:`dev-${version}-${changeId}`},
+   test: {namespace:'csnr-devops-lab-tools' , name: NAME, phase: 'test' , changeId:changeId, suffix: '-test'             , instance: `${NAME}-test`             , tag:`test-${version}`},
+   prod: {namespace:'csnr-devops-lab-tools' , name: NAME, phase: 'prod' , changeId:changeId, suffix: '-prod'             , instance: `${NAME}-prod`             , tag:`prod-${version}`}
 }
 
 module.exports = exports = phases
